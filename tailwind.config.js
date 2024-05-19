30d8c86f120f861b6795a699b37e548693e42af9
Nick Rabone <n.rabone72@aol.com>
2024-05-19 04:24:00 +0100

    Tailwind CSS setup and CSS file structure setup completed

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto Condensed", "sans-serif"],
+        caveat: ["Caveat", "cursive"],
+        amatic: ["Amatic SC", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
