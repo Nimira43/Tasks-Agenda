8d8aa6b9d3562536cb1674a08b32d145e8d0f810
Nick Rabone <n.rabone72@aol.com>
2024-05-20 00:53:51 +0100

    Working on input partial

--- index.js
+++ index.js
@@ -3,20 +3,26 @@ import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { engine } from 'express-handlebars'
 import handlebars from 'handlebars'
+import { readFileSync } from 'fs'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
 const PORT = 3000
 const app = express()
 
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
-app.set('views', [`${dirname(fileURLToPath(import.meta.url))}/views`])
+app.set('views', [`${__dirname}/views`])
 
 app.use(express.urlencoded({ extended: false }))
 
 app.use(express.static(`${dirname(fileURLToPath(import.meta.url))}/public`))
 
+const todoInput = handlebars.compile(readFileSync(`${__dirname}/views/partials/todo-input.handlebars`, "utf-8"))
+
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        partials: { todoInput },
+    })
 })
 
 app.listen(PORT, () => {
